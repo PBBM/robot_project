c0a7d9e7c3e4f8ecff42ffe8dd96787fe0e0fde3
PBBM <pierrebaptiste.bossard@gmail.com>
2021-01-25 18:18:02 +0100

    GET last news from 'News API' and save on database, success.

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Information = require('./models/information');
 const app = express();
 app.use(bodyParser.json());
 
+
+//====  Connexion à la base de données  ====================================================================================
 mongoose.connect('mongodb+srv://PBBM:351426@cluster0.lxbk2.mongodb.net/DbTestAnecdotes?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
@@ -14,6 +16,21 @@ mongoose.connect('mongodb+srv://PBBM:351426@cluster0.lxbk2.mongodb.net/DbTestAne
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+//===========================================================================================================================
+
+
+//============ Find ONE =====================================================================================================
+    // Information.find()
+    //     .then(anecdotes => console.log(anecdotes))
+    //     .catch(error => console.log({ error }))
+//===========================================================================================================================
+
+
+//============ Mise à zéro de la collection News ============================================================================
+Information.deleteMany({"description" : {$regex : ".*"+ " " +".*", $options: 'i'} })
+    .then(() => console.log('Objets supprimés ! Base de données remise à zéro'))
+    .catch(error => console.log(error));
+//===========================================================================================================================
 
 
 axios.get('http://newsapi.org/v2/top-headlines?sources=google-news-fr&apiKey=e923c49f647e42e0a579bf43378eba0b').then(resp => {
@@ -25,11 +42,16 @@ axios.get('http://newsapi.org/v2/top-headlines?sources=google-news-fr&apiKey=e92
             description: x.description,
             url: x.url
         })
-        // information.save();
+        information.save();
 
     }
 
 });
+
+
+
+
+
 
 //=== TEST de requêtage sur le serveur de l'app ====================
 app.use((req, res) => {
@@ -44,6 +66,6 @@ module.exports = app;
 
 
 
-//   =========    AXIOS librarie tutorial    =========
+//   =========    AXIOS library tutorial    =========
 //   |   http://zetcode.com/javascript/axios/        |
 //   =================================================
